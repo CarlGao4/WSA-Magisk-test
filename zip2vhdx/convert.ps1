0d2e27cdedbb00e2f8d2a11c28fb0daff5dc8041
CarlGao4 <gaoweiqicarl1@163.com>
2023-05-13 15:41:31 +0800

    fix typo

--- zip2vhdx/convert.ps1
+++ zip2vhdx/convert.ps1
@@ -2,8 +2,8 @@ $StartDir = $PWD
 Set-Location $PSScriptRoot
 
 foreach ($i in (Get-ChildItem ..\output)) {
-    foreach ($j in (Get-ChildItem)) {
-        Start-Process -Wait -NoNewWindow (Get-Process -Id $PID).Path ('.\zip2vhdx.ps1 "' + $j + '"')
+    foreach ($j in (Get-ChildItem $i)) {
+        Start-Process -Wait -NoNewWindow (Get-Process -Id $PID).Path ('.\zip2vhdx.ps1 "' + $j.FullName + '"')
     }
 }
 
