a17f76e83d4b317d63c369721cef866025081806
CarlGao4 <gaoweiqicarl1@163.com>
2023-05-13 10:36:07 +0800

    parallel fail-fast false

--- zip2vhdx/zip2vhdx.ps1
+++ zip2vhdx/zip2vhdx.ps1
@@ -14,12 +14,14 @@ $zipfile = (Get-ChildItem $zip)[0]
 
 Set-Location $PSScriptRoot
 
+Write-Host "Copying VHDX"
 Copy-Item -Path .\Blank-VHD.vhdx -Destination ("..\" + $zipfile.BaseName + ".vhdx") || exit 1
 $vhdx = (Get-ChildItem ("..\" + $zipfile.BaseName + ".vhdx"))[0]
 
 Remove-Item -Force -Recurse ..\mount
 New-Item -ItemType Directory -Path ".." -Name mount
 
+Write-Host "Mounting VHDX"
 'select vdisk file="' + $vhdx.FullName + '"' > diskpart.txt
 'attach vdisk noerr' >> diskpart.txt
 'select partition 2' >> diskpart.txt
@@ -31,17 +33,22 @@ if ($p.ExitCode -ne 0) {
     exit 1
 }
 
-$p = Start-Process -NoNewWindow -Wait "7z.exe" ('x "' + $zipfile.FullName + '" "..\' + $zipfile.BaseName + '"')
+Write-Host "Extracting file"
+$p = Start-Process -NoNewWindow -Wait "7z.exe" ('x "' + $zipfile.FullName + '" ..\extract')
 if ($p.ExitCode -ne 0) {
     exit 1
 }
 
-if (((Test-Path -Path ("..\" + $zipfile.BaseName + "\MakePri.ps1")) -eq $true) -and ((Test-Path -Path ("..\" + $zipfile.BaseName + "\MakePri.exe")) -eq $true)) {
+if (((Test-Path -Path ("..\extract\MakePri.ps1")) -eq $true) -and ((Test-Path -Path ("..\" + $zipfile.BaseName + "\MakePri.exe")) -eq $true)) {
+    Write-Host "Running MakePri.ps1"
     Start-Process -NoNewWindow -Wait (Get-Process -Id $PID).Path "-ExecutionPolicy Bypass -File MakePri.ps1" -WorkingDirectory ($PSScriptRoot + "\..")
 }
 
-Move-Item ("..\" + $zipfile.BaseName) -Destination "W:\"
+Write-Host "Moving files into VHDX"
+New-Item -ItemType Directory -Path ..\mount -Name $zipfile.BaseName
+Move-Item ..\extract\* -Destination ("..\mount\" + $zipfile.BaseName)
 
+Write-Host "Unmounting VHDX"
 'select vdisk file="' + $vhdx.FullName + '"' > diskpart.txt
 'select partition 2' >> diskpart.txt
 'attributes volume set readonly noerr' >> diskpart.txt
@@ -50,13 +57,14 @@ Start-Process -NoNewWindow -Wait "diskpart.exe" "/s diskpart.txt"
 
 ("artifact_name=" + $vhdx.Name) >> $env:GITHUB_OUTPUT
 ("artifact_path=" + $vhdx.FullName) >> $env:GITHUB_OUTPUT
+Write-Host ("Compressed size: " + $vhdx.Length)
 
 try {
-    Remove-Item -Force -Recurse diskpart.txt
+    Remove-Item -Force diskpart.txt
 }
 catch {}
 try {
-    Remove-Item -Force -Recurse ("..\" + $zipfile.BaseName)
+    Remove-Item -Force -Recurse ("..\extract")
 }
 catch {}
 Set-Location $StartDir
